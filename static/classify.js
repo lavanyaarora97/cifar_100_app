488fc0ae46a79aa4ba6200a6c93b70d7cf3cfe4c
Zedcodin <chinviajoshua51@gmail.com>
2024-05-07 04:27:18 +0200

    Add design file

--- static/classify.js
+++ static/classify.js
@@ -26,7 +26,7 @@ async function uploadImage() {
     //const fileInput = document.getElementById('uploadInput');
     formData.append('file', selectedFiles[0]);
 
-    const response = await fetch("http://127.0.0.1:10000/upload", {
+    const response = await fetch("https://cifar-100-app.onrender.com/upload", {
         method: 'POST',
         body: formData
     });
